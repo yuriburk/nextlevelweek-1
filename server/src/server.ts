c7f011c059cc5ab27206810953819c6605ec35f6
Yuri Burk <yuriburk@gmail.com>
2020-06-02 22:53:11 -0300

    feature: Created web application

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+  return response.send('Hello World');
+});
 
-  response.send('Hello World');
+app.post('/users', (request, response) => {
+  return response.json('Usuário');
 });
 
 app.listen(5000, () => console.log('🚀 Server started at port 5000'));
